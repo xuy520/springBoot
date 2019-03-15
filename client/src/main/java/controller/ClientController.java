bd4bc95dc42b8f8a6c11ec462059ed65aa9d5122
xuy520 <1152839182@qq.com>
2019-03-15 17:31:47 +0800

    高可用注冊中心

---
+++ client/src/main/java/controller/ClientController.java
@@ -0,0 +1,23 @@
+package controller;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author xuyue_2017@csii.com.cn
+ * @ClassName: ClientController
+ * @Description:
+ * @date 2018/12/13 20:45
+ * Copyright (c) CSII.
+ * All Rights Reserved.
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ClientController {
+    public static void main(String[] args) {
+        SpringApplication.run(ClientController.class, args);
+    }
+
+}
